68e5d5e318a1fd67048094ef25c5ef62198af5b6
Lucas Pulgaci <lucaspac032@hotmail.com>
2026-01-26 19:08:53 -0300

    refactor: include middlewareError in all controller catch blocks

--- controllers/topics.js
+++ controllers/topics.js
@@ -3,17 +3,17 @@ import { Topic } from "../models/AboutTopics.js";
 class TopicsController {
     static async listTopic(req,res,next){
         try{
-            const dados = await Topic.find({})
+            const dados = await Topic.find({})//.populate('topicsCode')
             res.status(200).send(dados)
         }catch(error){
             next(error)
         }
     }
 
-    static async listTopicById(req,res,id,next){
+    static async listTopicById(req,res,next){
         try{
             const id = req.params.id
-            const findData = await Topic.findById(id)
+            const findData = await Topic.findById(id)//.populate('topicsCode')
             res.status(200).send(findData)
         }catch(error){
             next(error)
@@ -22,6 +22,24 @@ class TopicsController {
     static async postTopic(req,res,next){
         try{
             const body = await Topic.create(req.body)
+            res.status(201).send(body)
+        }catch(error){
+            next(error)
+        }
+    }
+    static async putTopic(req,res,next){
+        try{
+            const id = req.params.id
+            const body = await Topic.findByIdAndUpdate(id, req.body, {new: true})
+            res.status(201).send(body)
+        }catch(error){
+            next(error)
+        }
+    }
+    static async patchTopic(req,res,next){
+         try{
+            const id = req.params.id
+            const body = await Topic.findByIdAndUpdate(id, req.body, {new: true})
             res.status(201).send(body)
         }catch(error){
             next(error)
