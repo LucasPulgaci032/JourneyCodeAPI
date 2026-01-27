a397a75713ce876685de74803b2f1ac75787a4ca
Lucas Pulgaci <lucaspac032@hotmail.com>
2026-01-27 14:56:25 -0300

    feat : create Topics database model

---
+++ models/AboutTopics.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+export const topic = new mongoose.Schema({
+        varAndConsts: {
+            type: [String],
+            //required: true
+    },
+        dataTypes : {
+             type: [String],
+             //required: true
+    },
+       objArr : {
+         type: String,
+         //required: true
+    },
+      ctrlEstructures: {
+        type: String,
+        //required: true
+    },
+      functions : {
+        type: String,
+        //required: true
+     },
+      domMann : {
+        type: String,
+       // required: true
+      },
+      topicsCode : [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "codes"
+      }]
+
+      
+
+
+})
+
+export const Topic = mongoose.model("topics", topic)
